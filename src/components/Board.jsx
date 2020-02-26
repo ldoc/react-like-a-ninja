4d403b9afab4a93791501eb41f7996bde1364063
ldoc <leandrodelolmo@gmail.com>
2020-02-26 21:46:14 +0100

    WIP

---
+++ src/components/Board.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Board = () => {
+    return <svg class='board'>
+        <circle cx="375" cy="300" r="100"></circle>
+    </svg>;
+}
---
+++ src/components/Game.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Board} from "./Board.jsx";
+
+export const Game = () => {
+    return <div class='game'>
+        <Board></Board>
+    </div>;
+}
